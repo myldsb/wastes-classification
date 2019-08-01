8476f2fc4124463e549ee9672d64580542eef119
xiaomi <shaomin2020yao@163.com>
2019-08-02 00:05:45 +0800

    add the index.html, show that wastes introduction

--- flaskp/__init__.py
+++ flaskp/__init__.py
@@ -1,18 +1,20 @@
 import os
-
 from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__)
-print("running path:===", app.root_path)
+def init_app():
+	app = Flask(__name__)
+	print("running path:===", app.root_path)
 
 
-@app.route('/', methods=('GET', 'POST'))
-def index():
-	return render_template('base.html')
+	@app.route('/', methods=('GET', 'POST'))
+	def index():
+		return render_template('index.html')
 
-from . import auth
-app.register_blueprint(auth.bp)
+	from . import auth
+	app.register_blueprint(auth.bp)
 
-from . import guide
-app.register_blueprint(guide.bp)
+	from . import guide
+	app.register_blueprint(guide.bp)
+
+	return app
--- run_app.py
+++ run_app.py
@@ -1,6 +1,8 @@
-from flaskp import app
+
+from flaskp import init_app
 
 if __name__ == '__main__':
+	app = init_app()
 	app.jinja_env.auto_reload = True
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
 	app.run(debug=True)

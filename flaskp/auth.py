365219ce6a48509c4b7d5a6cc724686ee06d2625
xiaomi <shaomin2020yao@163.com>
2019-08-04 15:34:45 +0800

    08.04 perfect the login page

--- flaskp/auth.py
+++ flaskp/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route('/')
+def index():
+	return	render_template('/auth/auth.html')
